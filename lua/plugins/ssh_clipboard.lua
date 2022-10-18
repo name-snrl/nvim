cb8a91d7a6afda45fdb22a4a9669b2cfcc7cbb8a
name_snrl <Demogorgon-74@ya.ru>
2022-10-18 15:34:38 +0500

    nvim-osc52: add a config load timer

--- lua/plugins/ssh_clipboard.lua
+++ lua/plugins/ssh_clipboard.lua
@@ -1,15 +1,19 @@
-if vim.g.loaded_clipboard_provider ~= 2 then
-  local function copy(lines, _)
-    require('osc52').copy(table.concat(lines, '\n'))
-  end
+vim.fn.timer_start(1000, function()
+  if vim.g.loaded_clipboard_provider ~= 2 then
+    local function copy(lines, _)
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
 
-  local function paste()
-    return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-  end
+    local function paste()
+      return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+    end
 
-  vim.g.clipboard = {
-    name = 'osc52',
-    copy = {['+'] = copy, ['*'] = copy},
-    paste = {['+'] = paste, ['*'] = paste},
-  }
-end
+    vim.g.clipboard = {
+      name = 'osc52',
+      copy = {['+'] = copy, ['*'] = copy},
+      paste = {['+'] = paste, ['*'] = paste},
+    }
+    vim.g.loaded_clipboard_provider = nil
+    vim.cmd 'runtime autoload/provider/clipboard.vim'
+  end
+end)

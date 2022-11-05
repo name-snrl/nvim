884c772fb37fa44e124d31b1598e20e161e5e7e7
name_snrl <Demogorgon-74@ya.ru>
2022-11-05 16:39:28 +0500

    lsp: add format mapping

--- lua/plugins/lspconfig/mapping.lua
+++ lua/plugins/lspconfig/mapping.lua
@@ -2,18 +2,18 @@
 -- free: d m n o p q r s u x y z
 
 local lsp = vim.lsp.buf
-local tel = Load'telescope.builtin'
-return  {
+local tel = Load 'telescope.builtin'
+return {
   gd = tel.lsp_definitions,
   gi = tel.lsp_implementations,
   gr = tel.lsp_references,
   gD = lsp.declaration,
   K  = lsp.hover,
   cv = lsp.rename,
+  gw = lsp.format,
   --['<C-k>']     = lsp.signature_help,
   --['<Leader>D']  = lsp.type_definition,
   --['<Leader>ca'] = lsp.code_action,
-  --['<Leader>f']  = lsp.format,
   --['<Leader>wa'] = lsp.add_workspace_folder,
   --['<Leader>wr'] = lsp.remove_workspace_folder,
   --['<Leader>wl'] = function()

01a7cb07e7cc8da551c62fa00fe4d72c905b059f
name_snrl <Demogorgon-74@ya.ru>
2022-11-05 20:46:42 +0500

    incline: hide incline if the 1st bufline is too long and is on the screen

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,4 +1,4 @@
-Load'incline'.setup {
+Load 'incline'.setup {
   window = {
     margin = { vertical = 0 },
     winhighlight = {
@@ -26,24 +26,19 @@ Load'incline'.setup {
     }
   },
   render = function(props)
-    -- TODO hide incline if the 1st bufline is too long and is on the screen
+    local res
     local buf = props.buf
     local win = props.win
-    local fn = vim.fn
-    local a = vim.api
-    local o = vim.opt_local
+    local name = vim.api.nvim_buf_get_name(buf)
 
-    local res
-    local name = a.nvim_buf_get_name(buf)
-
-    if a.nvim_buf_get_option(buf, 'buftype') == 'help' then
+    if vim.api.nvim_buf_get_option(buf, 'buftype') == 'help' then
       return vim.fs.basename(name)
-    elseif not a.nvim_buf_get_option(buf, 'buflisted') then
+    elseif not vim.api.nvim_buf_get_option(buf, 'buflisted') then
       return
-    elseif a.nvim_buf_get_option(buf, 'buftype') == 'nofile'
-      and name:match('man://')then
+    elseif vim.api.nvim_buf_get_option(buf, 'buftype') == 'nofile' and
+        name:match('man://') then
       return vim.fs.basename(name:gsub('man://', ''))
-    elseif a.nvim_buf_get_option(buf, 'buftype') == 'nofile' then
+    elseif vim.api.nvim_buf_get_option(buf, 'buftype') == 'nofile' then
       return
     elseif name ~= '' then
       res = vim.fn.fnamemodify(name, ':~:.')
@@ -51,76 +46,52 @@ Load'incline'.setup {
       res = '[No Name]'
     end
 
-    local ft = a.nvim_buf_get_option(buf, 'filetype')
+    local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
     if ft ~= '' then
       res = '[' .. ft .. ']' .. res
     end
 
-    if a.nvim_buf_get_option(buf, 'modified') then
+    if vim.api.nvim_buf_get_option(buf, 'modified') then
       res = res .. '[+]'
     end
 
-    local ro = a.nvim_buf_get_option(buf, 'readonly')
+    local ro = vim.api.nvim_buf_get_option(buf, 'readonly')
     if ro then
       res = res .. '[RO]'
     end
 
-    -- hide if the 1st line in buffer is too long
-    --local cursor_buf_pos = a.nvim_win_get_cursor(buf)[1]
-    --local cursor_win_pos = fn.screenrow()
-    --if o.winbar:get() ~= '' then -- winbar counts as a window row
-    --  cursor_win_pos = cursor_win_pos - 1
-    --end
-    --if cursor_buf_pos == cursor_win_pos then
-    --  local first_line = a.nvim_buf_get_lines(buf, 0, 1, false)[1]
-    --  local incline_opts = 5 -- margin + padding + 2 column for a good look
-    --  local num
-    --  local sign
-    --  --local fold -- I'm too lazy to calculate this
-
-    --  if o.number:get() or o.relativenumber:get() then
-    --    num = o.numberwidth:get()
-    --  else
-    --    num = 0
-    --  end
-
-    --  if o.signcolumn:get() then
-    --    -- I'm too lazy to calculate this
-    --    sign = 2
-    --  else
-    --    sign = 0
-    --  end
-
-    --  local length = ( #res + #first_line + incline_opts + num + sign )
-    --  if length > a.nvim_win_get_width(win) then
-    --    return
-    --  end
-    --end
-
-    local first_line = a.nvim_buf_get_lines(buf, 0, 1, false)[1]
-    local incline_opts = 5 -- margin + padding + 2 column for a good look
-    local num
-    local sign
-    --local fold -- I'm too lazy to calculate this
-
-    if o.number:get() or o.relativenumber:get() then
-      num = o.numberwidth:get()
-    else
-      num = 0
+    -- hide if the 1st line in buffer is too long and we see it
+    local function is_in_win()
+      return vim.api.nvim_win_get_cursor(0)[1] == vim.fn.winline()
     end
 
-    if o.signcolumn:get() then
-      -- I'm too lazy to calculate this
-      sign = 2
-    else
-      sign = 0
+    local function is_too_long()
+      local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+      local incline_opts = 5 -- margin + padding + 2 column for a good look
+      local num = 0
+      local sign
+      local fold = tonumber(vim.opt_local.foldcolumn:get()) or 0
+
+      if vim.opt_local.number:get() or vim.opt_local.relativenumber:get() then
+        num = vim.opt_local.numberwidth:get()
+      end
+
+      local s = vim.opt_local.signcolumn:get()
+      if s == 'number' and num ~= 0 then
+        sign = 0
+      elseif s == 'auto' or s == 'yes' or s == 'number' then
+        sign = 2
+      else
+        sign = s:sub(#s)
+      end
+
+      local length = (#res + #first_line + incline_opts + num + sign + fold)
+      return vim.api.nvim_win_get_width(win) < length
     end
 
-    local length = ( #res + #first_line + incline_opts + num + sign )
-    if length > a.nvim_win_get_width(win) then
+    if is_in_win() and is_too_long() then
       return
     end
-
     return res
   end,
 }

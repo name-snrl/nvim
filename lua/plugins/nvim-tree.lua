2b1707ba92e8eb0aa27f3084f947c000038edb19
name_snrl <Demogorgon-74@ya.ru>
2023-03-01 13:01:33 +0600

    nvim-tree: add mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,1 +1,9 @@
-Load 'nvim-tree'.setup { sync_root_with_cwd = true }
+Load 'nvim-tree'.setup {
+  sync_root_with_cwd = true,
+  on_attach = function(bufnr)
+    local api = Load 'nvim-tree.api'
+    Load 'core.utils'.set_maps({
+      ['<Space>'] = api.node.open.edit
+    }, { buffer = bufnr })
+  end
+}

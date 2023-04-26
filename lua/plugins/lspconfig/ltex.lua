a160d2f722c9a06346c9c0124a9fae6640edac43
name_snrl <Demogorgon-74@ya.ru>
2023-04-26 18:47:46 +0500

    lspconfig: enable underline for ltex diagnostics

--- lua/plugins/lspconfig/ltex.lua
+++ lua/plugins/lspconfig/ltex.lua
@@ -1,11 +1,4 @@
 Load 'lspconfig'.ltex.setup {
-  handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-      underline = false,
-    }
-    )
-  },
   settings = {
     ltex = {
       diagnosticSeverity = 'hint'

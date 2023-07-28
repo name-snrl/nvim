6dcfe1a889d74895966945a0128062ff8d086e69
name_snrl <Demogorgon-74@ya.ru>
2023-07-28 12:49:09 +0500

    core: cleanup

--- lua/core/abbreviations.lua
+++ lua/core/abbreviations.lua
@@ -1,10 +1,7 @@
 local abbr = {
   -- doc
   ['man'] = 'vert Man',
-  ['sman'] = 'Man',
   ['h'] = 'vert h',
-  ['sh'] = 'h',
-  ['help'] = 'vert help',
 }
 for k, v in pairs(abbr) do
   vim.cmd(string.format('cabbrev %s %s', k, v))
--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,17 +1,16 @@
-local o = vim.opt
 local f_name = 'core.autocmds.'
 
 Load('core.utils').create_autocmds {
   CmdlineEnter = {
     group = f_name .. 'wildmenu',
     callback = function()
-      o.smartcase = false
+      vim.opt.smartcase = false
     end,
   },
   CmdlineLeave = {
     group = f_name .. 'wildmenu',
     callback = function()
-      o.smartcase = true
+      vim.opt.smartcase = true
     end,
   },
 }

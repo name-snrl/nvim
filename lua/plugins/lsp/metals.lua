cb918dbfec0dff8da46b2df8182f039d04e6f3ba
name_snrl <Demogorgon-74@ya.ru>
2024-02-05 22:12:15 +0500

    metals: update cfg

--- lua/plugins/lsp/metals.lua
+++ lua/plugins/lsp/metals.lua
@@ -1,5 +1,14 @@
 local metals_config = require('metals').bare_config()
-metals_config.settings = { useGlobalExecutable = true }
+metals_config.settings = {
+  serverProperties = {
+    '-Xmx2G',
+    '-XX:+UseParallelGC',
+    '-Dmetals.verbose=true',
+    '-Dmetals.askToReconnect=false',
+    '-Dmetals.loglevel=debug',
+    '-Dmetals.build-server-ping-interval=10h',
+  },
+}
 metals_config.on_attach = Load('plugins.lsp.basics').on_attach
 local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {

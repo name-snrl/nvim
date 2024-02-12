4a8fb729006f0adbcb5d0d5543ee4d83e83b97b6
name_snrl <Demogorgon-74@ya.ru>
2024-02-12 19:51:08 +0500

    clipboard: set osc52 as the default clipboard

---
+++ lua/core/clipboard.lua
@@ -0,0 +1,13 @@
+-- since I use `nvim` exclusively in terminal and use `foot` which fully
+-- supports osc52 I see no point in using other clipboard providers
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}

c4992599da5186ef30540939c0a8088b2f67b159
name_snrl <Demogorgon-74@ya.ru>
2024-03-02 17:14:13 +0500

    flake: make top-level module more obvious

--- flake.nix
+++ flake.nix
@@ -17,88 +17,91 @@
 
   outputs =
     inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
-        inputs.pre-commit-hooks-nix.flakeModule
-        inputs.treefmt-nix.flakeModule
-      ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem =
-        { config, pkgs, ... }:
-        let
-          nvim = pkgs.callPackage ./wrapper.nix { };
-          selene-wrapped = pkgs.writeShellScriptBin "selene" ''
-            exec ${pkgs.selene}/bin/selene --config ${./selene}/selene.toml "$@"
-          '';
-        in
-        {
-          packages = {
-            inherit nvim selene-wrapped;
-          };
-          overlayAttrs = {
-            inherit nvim selene-wrapped;
-          };
-
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixfmt-rfc-style.enable = true;
-              deadnix.enable = true;
-              statix.enable = true; # fix, if possible
-              stylua.enable = true;
-              mdformat.enable = true;
-              mdformat.package = pkgs.mdformat.withPlugins (
-                p: with p; [
-                  mdformat-gfm
-                  mdformat-frontmatter
-                  mdformat-footnote
-                ]
-              );
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { inputs, ... }:
+      {
+        imports = with inputs; [
+          flake-parts.flakeModules.easyOverlay
+          pre-commit-hooks-nix.flakeModule
+          treefmt-nix.flakeModule
+        ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        perSystem =
+          { config, pkgs, ... }:
+          let
+            nvim = pkgs.callPackage ./wrapper.nix { };
+            selene-wrapped = pkgs.writeShellScriptBin "selene" ''
+              exec ${pkgs.selene}/bin/selene --config ${./selene}/selene.toml "$@"
+            '';
+          in
+          {
+            packages = {
+              inherit nvim selene-wrapped;
             };
-            settings.formatter.mdformat.options = [
-              "--wrap"
-              "80"
-            ];
-          };
+            overlayAttrs = {
+              inherit nvim selene-wrapped;
+            };
 
-          pre-commit.settings = {
-            hooks = {
-              treefmt.enable = true;
-              statix.enable = true; # check. not everything can be fixed, but we need to know what
-              selene = {
-                enable = true;
-                name = "selene";
-                description = "An opinionated Lua code linter";
-                entry = "${selene-wrapped}/bin/selene";
-                types = [ "lua" ];
+            treefmt = {
+              projectRootFile = "flake.nix";
+              programs = {
+                nixfmt-rfc-style.enable = true;
+                deadnix.enable = true;
+                statix.enable = true; # fix, if possible
+                stylua.enable = true;
+                mdformat.enable = true;
+                mdformat.package = pkgs.mdformat.withPlugins (
+                  p: with p; [
+                    mdformat-gfm
+                    mdformat-frontmatter
+                    mdformat-footnote
+                  ]
+                );
               };
-            };
-            settings.treefmt.package = config.treefmt.build.wrapper;
-          };
-
-          devShells.default =
-            with pkgs;
-            mkShellNoCC {
-              packages = [
-                (writeShellApplication {
-                  name = "update";
-                  text = ''
-                    nix flake update \
-                        --commit-lock-file \
-                        --inputs-from self \
-                        --override-input nixpkgs nixpkgs &&
-                        direnv reload
-                  '';
-                })
+              settings.formatter.mdformat.options = [
+                "--wrap"
+                "80"
               ];
-              shellHook = config.pre-commit.installationScript;
             };
-        };
-    };
+
+            pre-commit.settings = {
+              hooks = {
+                treefmt.enable = true;
+                statix.enable = true; # check. not everything can be fixed, but we need to know what
+                selene = {
+                  enable = true;
+                  name = "selene";
+                  description = "An opinionated Lua code linter";
+                  entry = "${selene-wrapped}/bin/selene";
+                  types = [ "lua" ];
+                };
+              };
+              settings.treefmt.package = config.treefmt.build.wrapper;
+            };
+
+            devShells.default =
+              with pkgs;
+              mkShellNoCC {
+                packages = [
+                  (writeShellApplication {
+                    name = "update";
+                    text = ''
+                      nix flake update \
+                          --commit-lock-file \
+                          --inputs-from self \
+                          --override-input nixpkgs nixpkgs &&
+                          direnv reload
+                    '';
+                  })
+                ];
+                shellHook = config.pre-commit.installationScript;
+              };
+          };
+      }
+    );
 }

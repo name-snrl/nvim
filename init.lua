3e6bfbc594857412d6c62506a98d58c42ee91298
name_snrl <Demogorgon-74@ya.ru>
2024-03-03 14:22:11 +0500

    init: configure `lazy.nvim`

--- init.lua
+++ init.lua
@@ -18,4 +18,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup 'plugins'
+require('lazy').setup {
+  spec = 'plugins',
+  defaults = { lazy = true },
+}

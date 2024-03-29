f8f2e0eb1e3f9f6f02115cd2007e92ce4d75742c
name_snrl <Demogorgon-74@ya.ru>
2024-03-30 00:02:36 +0500

    flake: inherit nixpkgs-lib from nixpkgs for flake-parts

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
       url = "github:neovim/neovim?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
